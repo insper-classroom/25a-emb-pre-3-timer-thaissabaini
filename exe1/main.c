0eadb24bc3210e9b6763084809adb79b597661ac
thaissabaini <thaissc3@al.insper.edu.br>
2025-03-10 11:55:58 -0300

    exe1

--- exe1/main.c
+++ exe1/main.c
@@ -6,12 +6,19 @@ const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
+volatile int g_timer_0 = 0;
+volatile int led_flag = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
         flag_f_r = 1;
     } else if (events == 0x8) { // rise edge
     }
+}
+
+bool timer_0_callback(repeating_timer_t *rt) {
+    g_timer_0 = 1;
+    return true; 
 }
 
 int main() {
@@ -26,10 +33,29 @@ int main() {
     gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
                                        &btn_callback);
 
-    while (true) {
+    int timer_0_hz = 2;
+    repeating_timer_t timer_0;
+    if (!add_repeating_timer_us(1000000 / timer_0_hz, 
+        timer_0_callback,
+        NULL, 
+        &timer_0)) {
+            printf("Failed to add timer\n");
+        }
 
+    while (true) {
         if (flag_f_r) {
             flag_f_r = 0;
+            led_flag = !led_flag;
+        }
+
+        if (g_timer_0) {
+            g_timer_0 = 0;
+            if (led_flag) {
+                gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+            } else {
+                gpio_put(LED_PIN_R, 0);
+            }
         }
     }
-}
+} 
+
