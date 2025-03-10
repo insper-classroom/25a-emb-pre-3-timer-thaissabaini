83e3e9bbf6e8a64700ba39326b757401fea34030
thaissabaini <thaissc3@al.insper.edu.br>
2025-03-10 12:45:17 -0300

    exe2

--- exe2/main.c
+++ exe2/main.c
@@ -11,6 +11,10 @@ const int LED_PIN_G = 6;
 volatile int flag_r = 0;
 volatile int flag_g = 0;
 
+volatile int g_timer_0 = 0;
+volatile int g_timer_1= 0;
+
+
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) {
         if (gpio == BTN_PIN_R)
@@ -20,12 +24,26 @@ void btn_callback(uint gpio, uint32_t events) {
     }
 }
 
+bool timer_0_callback(repeating_timer_t *rt) {
+    g_timer_0 = 1;
+    return true;
+}
+
+bool timer_1_callback(repeating_timer_t *rt) {
+    g_timer_1 = 1;
+    return true;
+}
+
 int main() {
+    stdio_init_all();
+
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_put(LED_PIN_R, 0);
 
     gpio_init(LED_PIN_G);
     gpio_set_dir(LED_PIN_G, GPIO_OUT);
+    gpio_put(LED_PIN_G, 0);
 
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
@@ -39,14 +57,45 @@ int main() {
                                        &btn_callback);
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
+    int timer_0_hz = 2;
+    int timer_1_hz = 4;
+    repeating_timer_t timer_0;
+    repeating_timer_t timer_1;
+
+    bool led_flag_r = 0;
+    bool led_flag_g = 0;
+
     while (true) {
 
         if (flag_r) {
             flag_r = 0;
+            led_flag_r = !led_flag_r;
+            if (led_flag_r) {
+                add_repeating_timer_us(1000000 / timer_0_hz, timer_0_callback, NULL, &timer_0);
+            } else {
+                cancel_repeating_timer(&timer_0);
+                gpio_put(LED_PIN_R, 0);
+            }
         }
 
         if (flag_g) {
             flag_g = 0;
+            led_flag_g = !led_flag_g;
+
+            if (led_flag_g) {
+                add_repeating_timer_us(1000000 / timer_1_hz, timer_1_callback, NULL, &timer_1);
+            } else {
+                cancel_repeating_timer(&timer_1);
+                gpio_put(LED_PIN_G, 0);
+            }
+        }
+
+        if (g_timer_0) {
+            g_timer_0 = 0;
+            gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+        }else if (g_timer_1) {
+            g_timer_1 = 0;
+            gpio_put(LED_PIN_G, !gpio_get(LED_PIN_G));
         }
     }
 }
