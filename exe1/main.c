3a21486a80f7e44c808edbfa1f60cea5fbbb5363
thaissabaini <thaissc3@al.insper.edu.br>
2025-03-10 12:11:00 -0300

    exe1

--- exe1/main.c
+++ exe1/main.c
@@ -17,14 +17,17 @@ void btn_callback(uint gpio, uint32_t events) {
 }
 
 bool timer_0_callback(repeating_timer_t *rt) {
-    g_timer_0 = 1;
-    return true; 
+    if (led_flag==1){
+        g_timer_0 =1;
+    }
+    return led_flag;
 }
 
 int main() {
     stdio_init_all();
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_put(LED_PIN_R, 0);
 
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
@@ -35,27 +38,24 @@ int main() {
 
     int timer_0_hz = 2;
     repeating_timer_t timer_0;
-    if (!add_repeating_timer_us(1000000 / timer_0_hz, 
-        timer_0_callback,
-        NULL, 
-        &timer_0)) {
-            printf("Failed to add timer\n");
-        }
 
     while (true) {
         if (flag_f_r) {
             flag_f_r = 0;
             led_flag = !led_flag;
-        }
 
-        if (g_timer_0) {
-            g_timer_0 = 0;
             if (led_flag) {
-                gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
-            } else {
+                add_repeating_timer_us(1000000 / timer_0_hz, timer_0_callback, NULL, &timer_0);
+            } else if (!led_flag){
+                cancel_repeating_timer(&timer_0);
                 gpio_put(LED_PIN_R, 0);
             }
         }
+
+        if (g_timer_0==1) {
+            g_timer_0 = 0;
+            gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+        }
     }
-} 
+}
 
